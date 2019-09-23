e9093dc5882592231ab7c1680029c093c55b3fd3
MailsonD <mailssondennis@gmail.com>
2019-09-23 17:45:42 -0300

    Finalizando listagem de pokemons

--- Pokedex/pokeproject/pokedex/views.py
+++ Pokedex/pokeproject/pokedex/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Pokemon
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, "pokedex/pokemon.html")
+    pokemon_list = Pokemon.objects.all()
+    return render(request, "pokedex/pokemon.html", {"pokemon_list": pokemon_list})
