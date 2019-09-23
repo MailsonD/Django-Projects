a2349aa5ec5adb3eab5bd8a47f85e0471880c55c
MailsonD <mailssondennis@gmail.com>
2019-09-23 18:52:53 -0300

    Adicionando busca por nome

--- Pokedex/pokeproject/pokedex/urls.py
+++ Pokedex/pokeproject/pokedex/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, buscar
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('buscar/', buscar),
 ]
--- Pokedex/pokeproject/pokedex/views.py
+++ Pokedex/pokeproject/pokedex/views.py
@@ -7,3 +7,12 @@ from .models import Pokemon
 def home(request):
     pokemon_list = Pokemon.objects.all()
     return render(request, "pokedex/pokemon.html", {"pokemon_list": pokemon_list})
+
+
+def buscar(request):
+    busca = request.POST.get("busca")
+    if busca:
+        pokemon_list = Pokemon.objects.filter(nome__contains=busca)
+    else:
+        pokemon_list = Pokemon.objects.all()
+    return render(request, "pokedex/pokemon.html", {"pokemon_list": pokemon_list})
